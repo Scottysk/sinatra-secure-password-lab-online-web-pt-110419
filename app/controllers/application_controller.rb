df22d9ae065a4566fdec9d95b5ee182dd3c31a74
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-03-04 22:29:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ class ApplicationController < Sinatra::Base
     user = User.find(session[:user_id])
     
     if user && user.authentication(params[:password])
-      session[:user_id]
+      session[:user_id] = user.id
   end
 
   get "/failure" do
