1f2c4e2636a3a670be627f4e98e6e753a45f6ab4
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-03-04 22:24:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    
-    if user.save
-      redirect to "/login"
-    else
-      redirect to "/failure"
-    end
+    if params
   end
 
   get '/account' do
