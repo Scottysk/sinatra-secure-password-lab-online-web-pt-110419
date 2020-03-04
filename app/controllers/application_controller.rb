31015460b9ce7a7b9e00e0df4f13761e289695fc
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-03-04 22:43:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(:username 
+    user = User.find_by(:username => params[:username])
     
     if user && user.authentication(params[:password])
       session[:user_id] = user.id
