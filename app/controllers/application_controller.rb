529de7f44dc5a118dd2a95b6a9b73e2281ee942b
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-03-04 23:07:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < Sinatra::Base
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/account"
-    else user.password == ""
+    else
       redirect "/failure"
     end
   end
